952bd8c14bced8391e7586ced1510acb6afcf45a
Colby Snedeker <colby_sned@sbcglobal.net>
2019-03-14 22:35:18 -0400

    Initial Commit

---
+++ hw1.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Colby Snedeker
+# CS 6900
+# HW 1
+
+# Get User input for first integer and place into array
+
+# Verify user wants to continue
+
+getUserInput () {
+    read -p "Enter an integer: " integer
+}
+
+getUserInput
+
